1fa82286785bc00559cdde05eca1f814fc75d868
NolanCustodio <nolanvongoop28@gmail.com>
2022-02-08 14:06:18 -0500

    react working with redux and react-redux

---
+++ my-site/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import './App.css';
+
+
+class App extends React.Component{
+    render(){
+        return(
+            <div className='col-xs-1 center-block'>
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+
+
+export default App;

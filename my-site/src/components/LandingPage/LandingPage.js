a791e90c92a5f5f52cf9e5e412649eda6e6d347a
NolanCustodio <nolanvongoop28@gmail.com>
2023-11-07 12:11:56 -0500

    changed alignment for resume to center of page

--- my-site/src/components/LandingPage/LandingPage.js
+++ my-site/src/components/LandingPage/LandingPage.js
@@ -5,7 +5,75 @@ import singleBlock from '../SingleBlock/singleBlock'
 const LandingPage = () => {
     return(
         <div>
-            pls
+            <div>
+                {/* Intro and small detail */}
+                <h1>
+                    Hello, I am Nolan!
+                </h1>
+                <p>
+                    I am a developer and I greatly thank you for taking the time to visit my site.
+                </p>
+            </div>
+
+            <div className="row">
+
+                <div className="card">
+                    <h1 className="col-sm">Github</h1>
+                    <div className="card-body">
+                        <a href="https://github.com/NolanCustodio" target="_blank" className="card-text stretched-link" rel="noopener noreferrer">
+                            Here is a link to my Github.
+                        </a>
+                    </div>
+                </div>
+
+                
+                <div className="card">
+                    <h1 className="col-sm">Resume</h1>
+                    <div className="card-body">
+                        <a href="/resume" target="_blank" className="card-text stretched-link" rel="noopener noreferrer">
+                            Here is a link to my Resume.
+                        </a>
+                    </div>
+                </div>
+                
+
+                {/* Github? */}
+            </div>
+            <div className="card">
+                <div className="row no-gutters">
+                    {/* This website name/image and small description */}
+                    <h1 className="col-md-3 card-body">
+                        Synchat
+                    </h1>
+                    <p className="col-md">
+                        This site also holds a small Web App that is currently a work in progress. <br/>
+                        Some of tools that I used to make what I have currently are: <br/>
+                        React, Nginx, Express, MySQL, and Docker. <br/>
+                        <a href="https://github.com/NolanCustodio/Synchat/tree/initialDeploy" className="stretched-link" target="_blank" rel="noopener noreferrer">
+                            Here is a link to this project if you would like to view this project on my Github.
+                        </a>
+                    </p>
+                </div>
+            </div>
+
+            <div className="card">
+                <div className="row no-gutters">
+                    {/* NewsBox github link and description */}
+                    <h1 className="col-md-3 card-body">
+                        NewsBox
+                    </h1>
+                    <p className="col-md">
+                        Another project that I created with a group while I was in University<br/>
+                        This was a LAMP Stack Application concept that used multiple Virtual Machines<br/>
+                        The project was more for teaching good techniques and tools such as:<br/>
+                        RabbitMQ, MVC Model, API Calls, Cron, Linux Services, File Storage, Logging, and more<br/>
+                        <a href="https://github.com/jcs78/News-Box-Application/tree/afterMidterm" className="stretched-link" target="_blank" rel="noopener noreferrer">
+                            This is link to the project
+                        </a>
+                    </p>
+                </div>
+            </div>
+
         </div>
     )  
 }
--- my-site/src/components/Resume/resume.js
+++ my-site/src/components/Resume/resume.js
@@ -5,16 +5,10 @@ import { Document, Page } from 'react-pdf';
 import resumePath from './NolanLeyCustodioResume.pdf'
 
 function Resume() {
-  const [numPages, setNumPages] = useState();
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
 
   return (
-    <div className='container'>
-      <Document file={resumePath} onLoadSuccess={onDocumentLoadSuccess}>
+    <div className='resumeContainer'>
+      <Document file={resumePath}>
         <Page 
             pageNumber={1} 
             renderAnnotationLayer={false}

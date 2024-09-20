24d64aee526d1b1c78b23886f0b15a9262768a4c
NolanCustodio <nolanvongoop28@gmail.com>
2024-09-20 12:05:43 -0400

    new landing cards should take less space

--- my-site/src/components/ProjectOverlay/overlay.js
+++ my-site/src/components/ProjectOverlay/overlay.js
@@ -7,16 +7,16 @@ import './Overlay.css';
 export default function Overlay(){
 
     return(
-        <div className='test-box'>
-            <div>
-                <h1>Nolan Ley Custodio</h1>
-                <div className='row landing-external-links'>
+        <div>
+            <h1>Nolan Ley Custodio</h1>
+            <div className='main-card'>
+                <div className='landing-external-links'>
                     <SingleResource link="https://github.com/NolanCustodio" name="Github"/>
                     <SingleResource link={resumePath} name="Resume"/>
                 </div>
-            </div>
-            <div>
-                
+                <div className='landing-right'>
+                    Projects
+                </div>
             </div>
         </div>
     )
--- my-site/src/components/Resources/SingleResource.js
+++ my-site/src/components/Resources/SingleResource.js
@@ -5,7 +5,7 @@ const SingleResource = (props) => {
     const resrouceName = props.name
     return(
         <div className="card my-card-landing">
-            <h1 className="col-sm link-title">{resrouceName}</h1>
+            <h1 className="link-title">{resrouceName}</h1>
             <div className="card-body">
                 <a href={resourceLink} target="_blank" className="card-text stretched-link resource-link" rel="noopener noreferrer">
                     Link to my {resrouceName}.

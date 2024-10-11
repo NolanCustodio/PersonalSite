9dd3d50fa9c9b5ea0968a6db2f5f38065f44575a
NolanCustodio <nolanvongoop28@gmail.com>
2024-10-11 12:03:57 -0400

    landing should populate information as user goes through projects

--- my-site/src/components/ProjectOverlay/overlay.js
+++ my-site/src/components/ProjectOverlay/overlay.js
@@ -20,7 +20,7 @@ export default function Overlay(){
             case 0:
                 return <SingleProjectBlock project={projects.portfolio}/>;
             case 1:
-                return <SingleProjectBlock project={projects.newbox}/>;
+                return <SingleProjectBlock project={projects.newsbox}/>;
             case 2:
                 return <SingleProjectBlock project={projects.synchat}/>;
             default:
@@ -29,41 +29,56 @@ export default function Overlay(){
     }
 
     const incrementState = () => {
-        console.log(projectIndex);
         if (projectIndex >= 2){
-            return
+            setProjectIndex(projectIndex => 0);
+            return;
         }
-        setProjectIndex(projectIndex + 1);
+        setProjectIndex(projectIndex => projectIndex + 1);
+    }
+
+    const decrementState = () => {
+        if (projectIndex <= 0){
+            setProjectIndex(projectIndex => 2);
+            return;
+        }
+        setProjectIndex(projectIndex => projectIndex - 1);
     }
 
     return(
         <div>
             <h1>Nolan Ley Custodio</h1>
             <div className='main-card'>
-                <div className='landing-left'>
-                    <div className='landing-external-links'>
-                        <SingleResource link="https://github.com/NolanCustodio" name="Github"/>
-                        <SingleResource link={resumePath} name="Resume"/>
+                <div className='card-top'>
+                    <div className='landing-left'>
+                        <div className='landing-external-links'>
+                            <SingleResource link="https://github.com/NolanCustodio" name="Github"/>
+                            <SingleResource link={resumePath} name="Resume"/>
+                        </div>
                     </div>
-                    <div>
-                        Name
+                    <div className='landing-right'>
+                        <div className='scrolling-project-container'>
+                            <div className='project-nav-buttons'>
+                                <button className='btn btn-primary' onClick={() =>{
+                                    decrementState();
+                                }}
+                                >{`<`}</button>
+                            </div>
+                            <div>
+                                <h2>Projects</h2>
+                                {currentProject(projectIndex)}
+                            </div>
+                            <div className='project-nav-buttons'>
+                                <button className='btn btn-primary' onClick={() => {
+                                    incrementState();
+                                }}>{`>`}</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='landing-right'>
-                    <div className='scrolling-project-container'>
-                        <div className='project-nav-buttons'>
-                            <button className='btn btn-primary'>left</button>
-                        </div>
-                        <div>
-                            <h2>Projects</h2>
-                            {currentProject(projectIndex)}
-                        </div>
-                        <div className='project-nav-buttons'>
-                            <button className='btn btn-primary' onClick={() => {
-                            incrementState();
-                        }}>right</button>
-                        </div>
-                    </div>
+                <div className='card-bottom'>
+                    <div>test</div>
+                    <div>test</div>
+                    <div>test</div>
                 </div>
             </div>
         </div>

0a56256f9f4b3da2bac0d99d1d69ae91134e0d8a
NolanCustodio <nolanvongoop28@gmail.com>
2025-01-06 14:43:30 -0500

    refactoring pages

--- my-site/src/components/LandingPage/LandingPage.js
+++ my-site/src/components/LandingPage/LandingPage.js
@@ -1,49 +1,17 @@
-//Node modules
-import { React } from 'react';
-import { useSelector } from 'react-redux';
+import MainCardTop from './MainCardSections/MainCardTop/MainCardTop';
+import MainCardBottom from './MainCardSections/MainCardBottom/MainCardBottom';
 
-//My Files
-import resumePath from '../Resume/NolanLeyCustodioResume.pdf'
-import SingleProjectBlock from '../Projects/Block/SingleProjectBlock';
-import SingleResource from '../Resources/SingleResource';
+import './LandingPage.css';
 
-import './LandingPage.css'
-
-const LandingPage = () => {
-
-    const projects = useSelector((state) => state.project.value);
+export default function Overlay(){
 
     return(
         <div>
-
-            <div className='intro-block'>
-                <h1 className='intro'>
-                    Hello, I am Nolan!
-                </h1>
-                {/* <p className='blurb'>
-                    I am a developer and I greatly thank you for taking the time to visit my site.
-                </p> */}
-            </div>
-            
-            <div className='landing-links'>
-                <h3 className='section-title'>My Information</h3>
-                <div className="row justify-content-around">
-                    <SingleResource link="https://github.com/NolanCustodio" name="Github"/>
-                    
-                    <SingleResource link={resumePath} name="Resume"/>
-                </div>
-            </div>            
-            <div>
-                <h2 className='section-title'>Projects</h2>
-
-                <SingleProjectBlock project={projects.portfolio}/>
-
-                <SingleProjectBlock project={projects.newsbox}/>
-
-                <SingleProjectBlock project={projects.synchat}/>
+            <h1>Nolan Ley Custodio</h1>
+            <div className='main-card'>
+                <MainCardTop/>
+                <MainCardBottom/>
             </div>
         </div>
-    )  
+    )
 }
-
-export default LandingPage;
---
+++ my-site/src/components/LandingPage/MainCardSections/MainCardTop/MainCardTop.js
@@ -0,0 +1,87 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import SingleResource from '../../../Resources/SingleResource';
+import resumePath from '../../../Resume/NolanLeyCustodioResume.pdf';
+import SingleProjectBlock from '../../../Projects/Block/SingleProjectBlock';
+
+// import { changeCount } from "../../../../features/projectCounter";
+import { incrementProjectIndex, decrementProjectIndex, resetProjectIndex } from "../../../../features/projectIndex";
+import { changeState } from "../../../../features/projectCarousel";
+
+import "./MainCard.css"
+
+const MainCardTop = () =>{
+    const projects = useSelector((state) => state.project.value);
+    // const projectCount = useSelector((state) => state.projectCounter.value);
+    const projectCount = 0;
+    // const projectIndex = useSelector((state) => state.projectIndex.value);
+    const projectCount0 = useSelector((state) => state.projectCarousel.index);
+
+    console.log(projectCount0);
+
+    const dispatch = useDispatch();
+
+    const currentProject = (projectCount) => {
+        switch(projectCount){
+            case 0:
+                return <SingleProjectBlock project={projects.portfolio}/>;
+            case 1:
+                return <SingleProjectBlock project={projects.newsbox}/>;
+            case 2:
+                return <SingleProjectBlock project={projects.synchat}/>;
+            default:
+                return null;
+        }   
+    }
+
+    const decrementState = () => {
+        if (projectCount0 <= 0){
+            dispatch(changeState(2));
+            return;
+        }
+        dispatch(changeState(projectCount0 - 1));
+    }
+
+
+    const incrementState = () => {
+        if (projectCount0 >= 2){
+            dispatch(changeState(0));
+            return;
+        }
+        dispatch(changeState(projectCount0 + 1));
+    }
+
+    return(
+        <div className='card-top'>
+            <div className='landing-left'>
+                <div className='landing-external-links'>
+                    <SingleResource link="https://github.com/NolanCustodio" name="Github"/>
+                    <SingleResource link={resumePath} name="Resume"/>
+                </div>
+            </div>
+            <div className='landing-right'>
+                <h2>Projects</h2>
+
+                    <div className='scrolling-project-container'>
+                        <div className='project-nav-buttons nav-left'>
+                            <button className='btn btn-primary' onClick={() =>{
+                                decrementState();
+                            }}
+                            >{`<`}</button>
+                        </div>
+                        <div className="card-container">
+                            {currentProject(projectCount)}
+                        </div>
+                        <div className='project-nav-buttons nav-right'>
+                            <button className='btn btn-primary' onClick={() => {
+                                incrementState();
+                            }}>{`>`}</button>
+                        </div>
+                    </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default MainCardTop;

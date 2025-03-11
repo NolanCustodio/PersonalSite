85af76235e48ec6824c5c62e95c1be809dea6e5a
NolanCustodio <nolanvongoop28@gmail.com>
2025-03-11 11:38:32 -0400

    quadrant reposition

--- my-site/src/components/LandingPage/MainCardSections/MainCardBottom/MainCardBottom.js
+++ my-site/src/components/LandingPage/MainCardSections/MainCardBottom/MainCardBottom.js
@@ -1,20 +1,13 @@
 import IconDisplay from "./IconDisplay";
+import ProjectCarrousel from "../MainCardTop/ProjectCarrousel/ProjectCarrousel";
 
 import "./MainCardBottom.css"
 
 const MainCardBottom = () => {
-    return(
+    return (
         <div className='card-bottom'>
-            <div className="bottom bottom-left">
-
-                <h1 className="static-text">Nolan Ley Custodio</h1>
-                <div className="email-text">
-                    <h2 className="static-text">custodionolan</h2>
-                    <h2 className="static-text">@gmail.com</h2>
-                </div>
-                <h2 className="static-text">Full Stack Developer</h2>
-            </div>
-            <IconDisplay/>
+            <ProjectCarrousel/>
+            <IconDisplay />
         </div>
     )
 }
--- my-site/src/components/LandingPage/MainCardSections/MainCardTop/MainCardTop.js
+++ my-site/src/components/LandingPage/MainCardSections/MainCardTop/MainCardTop.js
@@ -1,20 +1,26 @@
-import SingleResource from './Resources/SingleResource';
-import resumePath from '../../../Resume/NolanLeyCustodioResume.pdf';
-import ProjectCarrousel from "./ProjectCarrousel/ProjectCarrousel";
+import SingleResource from "./Resources/SingleResource";
+import resumePath from "../../../Resume/NolanLeyCustodioResume.pdf"
 
 
 import "./MainCardTop.css"
 import "./TopRightQuadrant.css"
 
 const MainCardTop = () => {
-    
+
     return (
         <div className='card-top'>
-            <div className='landing-left'>
+            <div className='landing-left top-left'>
+                <h1 className="static-text">Nolan Ley Custodio</h1>
+                <div className="email-text">
+                    <h2 className="static-text">custodionolan</h2>
+                    <h2 className="static-text">@gmail.com</h2>
+                </div>
+                <h2 className="static-text">Full Stack Developer</h2>
+            </div>
+            <div className="landing-left">
                 <SingleResource link="https://github.com/NolanCustodio" name="Github" />
                 <SingleResource link={resumePath} name="Resume" />
             </div>
-            <ProjectCarrousel/>
         </div>
     )
 }

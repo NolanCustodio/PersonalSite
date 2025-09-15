840199e6ce74320844d32e54838e53057a3137dd
Nolan Custodio <custodionolan@gmail.com>
2025-09-15 11:25:13 -0400

    added icons for resume and git hub need styling

--- my-site/src/components/FloatingButtons/FloatingButtons.js
+++ my-site/src/components/FloatingButtons/FloatingButtons.js
@@ -1,14 +1,26 @@
+import React from 'react';
+
 import './floating-button.css'
 
 import ResumePath from '../Resume/NolanLeyCustodioResume.pdf'
 
+import {ReactComponent as Github} from '../../images/resources/Github.svg';
+import {ReactComponent as Resume} from '../../images/resources/Resume.svg';
+
+
+const resourceIcons = {
+    Github: Github,
+    Resume: Resume,
+}
+
 const SingleButton = (props) => {
+    const Component = resourceIcons[props.name]
 
     return (
         <div className='single-button'>
             <a href={props.path} target="_blank" rel="noreferrer">
                 <button>
-                    {props.name}
+                    <Component/>
                 </button>
             </a>
         </div>
